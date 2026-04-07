8f33005e2ce88cc0172eba1daa4cce217735ad97
ParkChanWoo0321 <op9563_@naver.com>
2026-04-07 22:22:29 +0900

    2주차 코드

--- src/main/java/com/example/simplememo/controller/MemoController.java
+++ src/main/java/com/example/simplememo/controller/MemoController.java
@@ -6,8 +6,12 @@ import com.example.simplememo.dto.MemoResponseDto;
 import com.example.simplememo.entity.Memo;
 import com.example.simplememo.service.MemoService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -26,6 +30,58 @@ public class MemoController {
                         .statusCode(200)
                         .msg("메모 생성이 완료되었습니다.")
                         .data(response)
+                        .build());
+    }
+
+    //메모 단건 조회
+    @GetMapping("/{memoId}")
+    public ResponseEntity<CommonResponse<MemoResponseDto>> getMemo(@PathVariable Long memoId) {
+        Memo memo = memoService.getMemo(memoId);
+        MemoResponseDto response = new MemoResponseDto(memo);
+        return ResponseEntity.ok()
+                .body(CommonResponse.<MemoResponseDto>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .msg("단건 조회가 완료 되었습니다.")
+                        .data(response)
+                        .build());
+    }
+
+    //메모 전체 조회
+    @GetMapping
+    public ResponseEntity<CommonResponse<List<MemoResponseDto>>> getMemos() {
+        List<Memo> memos = memoService.getMemos();
+        List<MemoResponseDto> response = memos.stream()
+                .map(MemoResponseDto::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok()
+                .body(CommonResponse.<List<MemoResponseDto>>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .msg("목록 조회가 완료 되었습니다.")
+                        .data(response)
+                        .build());
+    }
+
+    @PutMapping("/{memoId}")
+    public ResponseEntity<CommonResponse<MemoResponseDto>> updateMemo(
+            @PathVariable Long memoId,
+            @RequestBody MemoRequestDto request) {
+        Memo memo = memoService.updateMemo(memoId, request);
+        MemoResponseDto response = new MemoResponseDto(memo);
+        return ResponseEntity.ok()
+                .body(CommonResponse.<MemoResponseDto>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .msg("메모 수정이 완료되었습니다.")
+                        .data(response)
+                        .build());
+    }
+
+    @DeleteMapping("/{memoId}")
+    public ResponseEntity<CommonResponse<Void>> deleteMemo(@PathVariable Long memoId) {
+        memoService.deleteMemo(memoId);
+        return ResponseEntity.ok()
+                .body(CommonResponse.<Void>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .msg("메모 삭제가 완료되었습니다.")
                         .build());
     }
 }
--- src/main/java/com/example/simplememo/service/MemoService.java
+++ src/main/java/com/example/simplememo/service/MemoService.java
@@ -4,7 +4,11 @@ import com.example.simplememo.dto.MemoRequestDto;
 import com.example.simplememo.entity.Memo;
 import com.example.simplememo.repository.MemoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -15,5 +19,28 @@ public class MemoService {
     public Memo createMemo(MemoRequestDto request) {
         Memo memo = request.toEntity();
         return memoRepository.save(memo);
+    }
+
+    //메모 단건 조회
+    public Memo getMemo(Long memoId) {
+        return memoRepository.findById(memoId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 메모가 존재하지 않습니다."));
+    }
+
+    // 메모 전체 조회
+    public List<Memo> getMemos() {
+        return memoRepository.findAll();
+    }
+
+    public Memo updateMemo(Long memoId, MemoRequestDto request) {
+        Memo memo = getMemo(memoId); // 메모 단건 조회 기능을 이용하여 메모가 데이터베이스에 실존하는지 확인
+        memo.setTitle(request.getTitle());
+        memo.setContent(request.getContent());
+        return memoRepository.save(memo); // Repository의 save 메소드를 이용하여 수정된 메모를 데이터베이스에 저장
+    }
+
+    public void deleteMemo(Long memoId) {
+        Memo memo = getMemo(memoId); // 메모 단건 조회 기능을 이용하여 메모가 데이터베이스에 실존하는지 확인
+        memoRepository.delete(memo); // Repository의 delete 메소드를 이용하여 메모를 데이터베이스에서 삭제
     }
 }
